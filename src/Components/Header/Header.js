bcfdafa47a5c0707f6b7f9dc48bc8db533f49f16
NhuNhy <82555469+xrnhy2911@users.noreply.github.com>
2023-11-07 16:24:26 +0700

    #2 Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,35 @@
 import React from 'react'
-import"./Header.css"
+import "./Header.css"
 export default function Header() {
   return (
-    <div className='Header-main'>
-        <ul class="dropdown-menu">
-        <li><h6 class="dropdown-header">Dropdown header</h6></li>
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-   </ul>
+    <div >
+      <div class="headerWrapper">
+        <div class="headerLogo">
+          <img class="imgLogo" src='./images/Logo/1.png' alt='logo PAM PAM '></img>
+        </div>
+        <div class="headerMenu">
+          <div class="subMenuTop">
+              
+          </div>
+          <div class="subMenuBot">
+            <div class="seachBar">
+              <div class="input-group border-input">
+                <div class="form-outline">
+                  <input type="search" id="form1" class="form-control css-form "  placeholder='Search'/>
+                </div>
+                <a type="button" class="css-button-a" href='#'>
+                  <img class="searchIcon" src='./images/Icon/search.svg'></img>
+                </a>
+              </div>
+            </div>
+            <div class="headerCart">
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   )
 }
