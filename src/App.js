9780cbe8ab8c40ac4a20eec5ae082b6f35df77d1
PaTuanas <91712990+PaTuanas@users.noreply.github.com>
2023-11-10 01:06:47 +0700

    Up#2
    Item Component (Undone)

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Categories from './Components/Categories/Categories';
 import Profile from './Components/Profile/Profile';
+import Item from './Components/Items/Item'
 function App() {
   return (
     <div className="App">
-      <Profile/>
+      <Item/>
     </div>
   );
 }
