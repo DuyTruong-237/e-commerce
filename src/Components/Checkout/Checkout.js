96d6c2dc9abb2ca295c1b72989011ad44906bf11
NhuNhy <82555469+xrnhy2911@users.noreply.github.com>
2023-11-26 17:47:08 +0700

    #5 PAGE CHECKOUT
    DONE

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './Checkout.css'
 import '../Cart/ActiveCart.css'
+import CheckoutInfo from './CheckoutInfo'
 export default function Checkout(){
     return(
         <div class="checkoutWrapper" id='checkoutWrapperId' >
@@ -8,9 +9,19 @@ export default function Checkout(){
                 <div class="AC-Header">
                     <h1 class="AC-Text">Checkout <span class="quantityItems">(2 item)</span> </h1>
                 </div>
-                <div class="AC-Body">
-                    
-
+                <div class="AC-Body ">
+                        <form class="formCheckout">
+                            <div class="CheckoutWrapperContent">
+                        <div class="CheckoutLeft bgWhite">
+                            <CheckoutInfo/>
+                        </div>
+                       
+                        <div class="CheckoutRight bgWhite">
+                        right
+                        </div>
+                        </div>
+                        </form>
+                
                 </div>
             </div>
         </div>
