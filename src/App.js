ca737e61d891e03a7322efc6c75526b59d644526
PaTuanas <91712990+PaTuanas@users.noreply.github.com>
2023-11-27 20:22:46 +0700

    Up#4
    ItemDescription

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import Header from './Components/Header/Header';
 import Categories from './Components/Categories/Categories';
 import Profile from './Components/Profile/Profile';
 import Item from './Components/Items/Item'
+import BriefShop from './Components/BriefShop/BriefShop';
+import Home from './Screens/Home/Home'
+import itemDes from './Components/Item-Description/ItemDes'
+
 function App() {
   return (
     <div className="App">
-      <Item/>
+      <itemDes/>
     </div>
   );
 }
---
+++ src/Components/Item-Description/ItemDes.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function  () {
+  return (
+    <div className='itemDescription-Wrapper'>
+        <div className='itemDes-Title'>
+            
+        </div>
+        <div className='itemDes-Content'>
+
+        </div>
+    </div>
+  )
+}
