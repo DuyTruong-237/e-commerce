eae42369ad2bfda915e9088fd4ccbf1cf5882dda
PaTuanas <91712990+PaTuanas@users.noreply.github.com>
2023-12-01 23:58:51 +0700

    Up#8
    Order Screen(Done) and Responsive Home Screen(Undone)

--- src/App.js
+++ src/App.js
@@ -3,39 +3,33 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Screens/Home/Home'
 import Login from './Screens/Login/Login'
 import Profile from './Screens/Profile/Profile';
-import ListItem from './Components/ListItem-Cart/ListItem';
+import OwnOrder from './Screens/OwnOrder/OwnOrder';
 import ProductItem from './Components/Product/ProductItem';
 import ItemProducts from './Components/ItemProducts/ItemProducts';
 import ActiveCart from './Components/Cart/ActiveCart'
 import BuyItem from './Screens/InfoProduct/InfoProduct'
 import DailyDiscover from './Screens/DailyDiscover/DailyDiscover';
 import SearchResult from './Screens/SearchResult/SearchResult';
-import Shop from './Screens/Shop/Shop';
-import AddNewProduct from './Components/AddNewProduct/AddNewProduct';
-import UpdateProduct from './Components/UpdateProduct/UpdateProduct';
 import "./App.css"
 
 function App() {
   return (
     <Router>
       <div className="App">
-        {/* <Routes>
+        <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/login" element={<Login />}/>
           <Route path="/profile" element={<Profile />}/>
-          <Route path="/list" element={<ListItem />}/>
+          <Route path="/order" element={<OwnOrder />}/>
           <Route path="/cart" element={<ActiveCart />}/>
           <Route path="/inforproduct" element={<BuyItem />}/>
           <Route path="/SearchResult" element={<SearchResult />}/>
           <Route path="/discover" element={<DailyDiscover />}/>
-          <Route path="/shop" element={<Shop />}/>
-
 
 
 
         </Routes>
-         */}
-         <UpdateProduct/>
+        
       </div>
     </Router>
   );
