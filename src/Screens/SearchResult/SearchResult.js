239ed460ca767e7defcb1b4669c2ab8a95bc928f
PaTuanas <91712990+PaTuanas@users.noreply.github.com>
2023-12-02 12:42:01 +0700

    Up#9
    Responsive some screen

--- src/Screens/SearchResult/SearchResult.js
+++ src/Screens/SearchResult/SearchResult.js
@@ -1,18 +1,30 @@
 import React from 'react'
 import "./SearchResult.css"
 import Header from '../../Components/Header/Header'
+import ProductItem from '../../Components/Product/ProductItem'
+import NumberTab from '../../Components/NumberTab/NumberTab'
+import Footer from '../../Components/Footer/Footer'
 export default function SearchResult() {
   return (
     <div className='SR-body'>
-      <Header/>
+      <Header />
       <div className='SR-container'>
-        <div className='SR-title'>Kết quả tìm kiếm cho "Áo Khoác"</div>
+        <div className='SR-title'>Search results for "Lipstick"</div>
         <div className='SR-content'>
-        
-
+        <ProductItem/>
+        <ProductItem/>
+        <ProductItem/>
+        <ProductItem/>
+        <ProductItem/>
+        <ProductItem/>
+        <ProductItem/>
+        <ProductItem/>
+        <ProductItem/>
+        <ProductItem/>
         </div>
-
+        <NumberTab/>
       </div>
+      <Footer />
     </div>
   )
 }
